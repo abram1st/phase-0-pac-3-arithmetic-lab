01b47d591536993b583eb5f7881fc036d5ff235f
abram1st <abram.luntungan97@gmail.com>
2021-04-15 03:39:55 -0600

    completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add() {
+
+}
+function subtract() {
+    
+}
+function multiply() {
+    
+}
+function divide() {
+
+}
+function increment(n) {
+
+}
+function decrement(n) {
+
+}
+function makeint(string) {
+
+}
+function preserveDecimal(string) {
+    
+}
